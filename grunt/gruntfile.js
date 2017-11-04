6e1e3bfb80042f4ff3a7d6e944458b038a75fe6d
Umair Abbasi <umair.abbasi@fourfridays.com>
2017-11-04 14:33:37 +0300

    added js compression to reduce filesize

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -12,8 +12,7 @@ module.exports = function(grunt) {
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-                compress: {},
-                beautify: true
+                compress: true
         },
         dist: {
             files: {
