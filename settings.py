cd2158386ec33edd1dfcef8e081d687d05f29fb1
Umair Abbasi <umair.abbasi@fourfridays.com>
2025-12-29 20:24:25 -0500

    oops

--- settings.py
+++ settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "wagtail.contrib.legacy.sitemiddleware.SiteMiddleware",
     "wagtail.contrib.redirects.middleware.RedirectMiddleware",
+]
 
 sentry_dsn = os.environ.get("SENTRY_DSN", "")
 if sentry_dsn:
